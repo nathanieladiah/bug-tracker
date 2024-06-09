fdf251254b03f7c4c6295d7a1cd82404dfd9ad70
nathanieladiah <nathanieladiah@gmail.com>
2024-06-09 11:33:45 -0400

    Move auth templates and use built-in Django views

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
 	# path('signup/', SignUpView.as_view(), name='signup'),
 	path('register/', views.register, name='signup'),
-	path('logout/', views.logout_view, name='logout'),
-	path('login/', views.login_view, name='login'),
+	# path('logout/', views.logout_view, name='logout'),
+	# path('login/', views.login_view, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -26,7 +26,7 @@ def register(request):
 		confirmation = request.POST['confirmation']
 
 		if password != confirmation:
-			return render(request, 'accounts/register.html', {
+			return render(request, 'registration/register.html', {
 				'message': 'Passwords must match.'
 			})
 
@@ -35,41 +35,41 @@ def register(request):
 			user = User.objects.create_user(username, email, password)
 			user.save()
 		except IntegrityError:
-			return render(request, 'accounts/register.html', {
+			return render(request, 'registration/register.html', {
 				'message': 'Username already taken'
 			})
 
 		login(request, user)
 		return HttpResponseRedirect(reverse('dashboard'))
 	else:
-		return render(request, 'accounts/register.html')
+		return render(request, 'registration/register.html')
 
 
 
-def login_view(request):
-	if request.method == 'POST':
-		# Attempt to sign user in
-		username = request.POST['username']
-		password = request.POST['password']
-		user = authenticate(request, username=username, password=password)
-		next_value = request.POST.get('next')
-		print(next_value)
+# def login_view(request):
+# 	if request.method == 'POST':
+# 		# Attempt to sign user in
+# 		username = request.POST['username']
+# 		password = request.POST['password']
+# 		user = authenticate(request, username=username, password=password)
+# 		next_value = request.POST.get('next')
+# 		print(next_value)
 
-		# Check if authentication is successful
-		if user is not None:
-			login(request, user)
-			if next_value == '':
-				return HttpResponseRedirect(reverse('dashboard'))
-			else:
-				return redirect(next_value)
-		else:
-			return render(request, 'accounts/login.html', {
-				'message': 'Invalid username and/or password.'
-			})
+# 		# Check if authentication is successful
+# 		if user is not None:
+# 			login(request, user)
+# 			if next_value == '':
+# 				return HttpResponseRedirect(reverse('dashboard'))
+# 			else:
+# 				return redirect(next_value)
+# 		else:
+# 			return render(request, 'accounts/login.html', {
+# 				'message': 'Invalid username and/or password.'
+# 			})
 
-	return render(request, 'accounts/login.html')
+# 	return render(request, 'accounts/login.html')
 
 
-def logout_view(request):
-	logout(request)
-	return HttpResponseRedirect(reverse('home'))
+# def logout_view(request):
+# 	logout(request)
+# 	return HttpResponseRedirect(reverse('home'))
