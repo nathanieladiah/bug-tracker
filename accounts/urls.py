c42d59a074fd66e9a1d047fd856206a5689f00c5
nathanieladiah <nathanieladiah@gmail.com>
2024-06-09 10:04:46 -0400

    Add login, register, and logout pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import SignUpView
+# from .views import SignUpView
+from . import views
 
 urlpatterns = [
-	path('signup/', SignUpView.as_view(), name='signup'),
+	# path('signup/', SignUpView.as_view(), name='signup'),
+	path('register/', views.register, name='signup'),
+	path('logout/', views.logout_view, name='logout'),
+	path('login/', views.login_view, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,75 @@
-from django.urls import reverse_lazy
+from django.conf import settings
+from django.contrib.auth import authenticate, get_user_model, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView
 
 from .forms import CustomUserCreationForm
 
+# User = settings.AUTH_USER_MODEL
+User = get_user_model()
 
-class SignUpView(CreateView):
-	form_class = CustomUserCreationForm
-	success_url = reverse_lazy('login')
-	template_name = 'registration/signup.html'
+# class SignUpView(CreateView):
+# 	form_class = CustomUserCreationForm
+# 	success_url = reverse_lazy('login')
+# 	template_name = 'registration/signup.html'
+
+def register(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		email = request.POST['email']
+
+		# Ensure password matches confirmation
+		password = request.POST['password']
+		confirmation = request.POST['confirmation']
+
+		if password != confirmation:
+			return render(request, 'accounts/register.html', {
+				'message': 'Passwords must match.'
+			})
+
+		# Attempt to create new user
+		try:
+			user = User.objects.create_user(username, email, password)
+			user.save()
+		except IntegrityError:
+			return render(request, 'accounts/register.html', {
+				'message': 'Username already taken'
+			})
+
+		login(request, user)
+		return HttpResponseRedirect(reverse('dashboard'))
+	else:
+		return render(request, 'accounts/register.html')
+
+
+
+def login_view(request):
+	if request.method == 'POST':
+		# Attempt to sign user in
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		next_value = request.POST.get('next')
+		print(next_value)
+
+		# Check if authentication is successful
+		if user is not None:
+			login(request, user)
+			if next_value == '':
+				return HttpResponseRedirect(reverse('dashboard'))
+			else:
+				return redirect(next_value)
+		else:
+			return render(request, 'accounts/login.html', {
+				'message': 'Invalid username and/or password.'
+			})
+
+	return render(request, 'accounts/login.html')
+
+
+def logout_view(request):
+	logout(request)
+	return HttpResponseRedirect(reverse('home'))
